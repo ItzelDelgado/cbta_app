83089fb5c64a37fd7a0e429e2d93a2aaf3b0978b
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2024-01-06 03:20:04 -0600

    Se agrego el mod admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,13 @@
 <?php 
 use Illuminate\Support\Facades\Route; //Importamos para generar nuestras rutas.
 //Debemos avisarle a laravel que hemos creado un nuevo archivo de rutas en providers
-Route::get('/admin', function(){
-    return "Hola desde el administrador";
-});
+Route::get('/', function(){
+    session()->flash('swal', [
+        'icon'=>"error",
+        'title'=>"Oops...",
+        'text'=>"Something went wrong :(!",
+        'footer'=>'<a href="#">Why do I have this issue?</a>'
+]);
+    return view('admin.dashboard');
+})->name('admin.dashboard');
 
