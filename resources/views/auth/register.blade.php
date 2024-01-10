3118b88c5abd7a1bf714e45b84187938b6a0693c
ItzelDelgado <itzeldelgado.ciencias@gmail.com>
2024-01-09 22:56:06 -0600

    Se agreggo la tabla hospitals y se modifico el login eliminando el email

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,13 +10,12 @@
             @csrf
 
             <div>
-                <x-label for="name" value="{{ __('Name') }}" />
+                <x-label for="name" value="{{ __('Nombre') }}" />
                 <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
-
             <div class="mt-4">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
+                <x-label for="name" value="{{ __('Nombre de Usuario') }}" />
+                <x-input id="name" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
             </div>
 
             <div class="mt-4">
